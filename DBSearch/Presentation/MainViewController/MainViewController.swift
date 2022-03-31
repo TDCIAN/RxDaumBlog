5592aa67e34b8e5e4c0a0dd1aeee3c22e9ace86b
TDCIAN <tdcian71@gmail.com>
2022-03-31 19:23:04 +0900

    bind

--- DBSearch/Presentation/MainViewController/MainViewController.swift
+++ DBSearch/Presentation/MainViewController/MainViewController.swift
@@ -29,7 +29,75 @@ class MainViewController: UIViewController {
     }
     
     func bind() {
-        blogListTableView.headerView.sortButtonTapped
+        let blogResult = searchBar.shouldLoadResult
+            .flatMapLatest { query in
+                SearchBlogNetwork().searchBlog(query: query)
+            }
+            .share()
+        
+        let blogValue = blogResult
+            .compactMap { result -> DKBlog? in
+                guard case .success(let blogData) = result else {
+                    return nil
+                }
+                return blogData
+            }
+        
+        let blogError = blogResult
+            .compactMap { result -> String? in
+                guard case .failure(let error) = result else {
+                    return nil
+                }
+                return error.localizedDescription
+            }
+        
+        // 네트워크를 통해 가져온 값을 cellData로 변환
+        let cellData = blogValue
+            .map { blog -> [BlogListCellData] in
+                return blog.documents
+                    .map { document in
+                        let thumbnailURL = URL(string: document.thumbnail ?? "")
+                        return BlogListCellData(
+                            thumbnailURL: thumbnailURL,
+                            name: document.name,
+                            title: document.title,
+                            datetime: document.datetime
+                        )
+                    }
+            }
+        
+        // FilterView를 선택했을 때 나오는 alertSheet를 선택했을 때 type
+        let sortedType = alertActionTapped
+            .filter {
+                switch $0 {
+                case .title, .datetime:
+                    return true
+                default:
+                    return false
+                }
+            }
+            .startWith(.title) // 기본 값은 title로 하겠다
+        
+        // MainViewController -> blogListTableView
+        Observable
+            .combineLatest(
+                sortedType,
+                cellData
+            ) { type, data -> [BlogListCellData] in
+                switch type {
+                case .title:
+                    return data.sorted { $0.title ?? "" < $1.title ?? "" }
+                case .datetime:
+                    return data.sorted { $0.datetime ?? Date() > $1.datetime ?? Date() }
+                default:
+                    return data
+                }
+            }
+            .bind(to: blogListTableView.cellData)
+            .disposed(by: disposeBag)
+        
+        
+        let alertSheetForSorting = blogListTableView.headerView.sortButtonTapped
             .map { _ -> Alert in
                 return Alert(
                     title: nil,
@@ -38,6 +106,22 @@ class MainViewController: UIViewController {
                     style: .actionSheet
                 )
             }
+        
+        let alertForErrorMessage = blogError
+            .map { message -> Alert in
+                return Alert(
+                    title: "앗!",
+                    message: "예상치 못한 오류가 발생했습니다. 잠시후 다시 시도해주세요.\n\(message)",
+                    actions: [.confirm],
+                    style: .alert
+                )
+            }
+        
+        Observable
+            .merge(
+                alertSheetForSorting,
+                alertForErrorMessage
+            )
             .asSignal(onErrorSignalWith: .empty())
             .flatMapLatest { alert -> Signal<AlertAction> in
                 let alertController = UIAlertController(
