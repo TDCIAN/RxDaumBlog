e56f54a48d8ac1fc754f0cd01bc32dfee76195c8
TDCIAN <tdcian71@gmail.com>
2022-03-31 15:52:14 +0900

    config network service

---
+++ DBSearch/Network/SearchBlogAPI.swift
@@ -0,0 +1,27 @@
+//
+//  SearchBlogAPI.swift
+//  DBSearch
+//
+//  Created by JeongminKim on 2022/03/31.
+//
+
+import Foundation
+
+struct SearchBlogAPI {
+    static let scheme = "https"
+    static let host = "dapi.kakao.com"
+    static let path = "/v2/search/"
+    
+    func searchBlog(query: String) -> URLComponents {
+        var components = URLComponents()
+        components.scheme = SearchBlogAPI.scheme
+        components.host = SearchBlogAPI.host
+        components.path = SearchBlogAPI.path + "blog"
+        
+        components.queryItems = [
+            URLQueryItem(name: "query", value: query)
+        ]
+        
+        return components
+    }
+}
